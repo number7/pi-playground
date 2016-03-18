a6cc97dd8432bfa38635e7de0c7da9ef3819631a
sodajusticewarriors@gmail.com <Fr33TheCans!>
2016-03-17 22:07:10 -0500

    updated main.py to add shell command example

--- main.py
+++ main.py
@@ -4,6 +4,17 @@
 #--------------------------------------
 import sys
 import lcd
+from subprocess import call
+
+LINE_1 = 1
+LINE_2 = 2
+
+# shell out to C program and grab the return value
+return_code = call("./dummyApp", shell=True)
+print return_code
 
 # calling the function to initialize the LCD
-lcd.countdown(15)
+##lcd.countdown(5, "Refill the soda!")
+##
+##timeToDisplay=5
+##lcd.sendText("Shame on you!",LINE_1,timeToDisplay)
